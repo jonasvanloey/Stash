29f0fb044d613cb05493a3d7b22ae530018b3f9d
Arteau De Meester <arteaudemeester@gmail.com>
2018-01-21 14:57:08 +0100

    nieuw logo toegevoegd, routes aangepast

--- webapp/routes/web.php
+++ webapp/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
 Route::get('/add-barcode','barcodeController@index');
 Route::get('/barcode/{id}/delete','barcodeController@delete');
