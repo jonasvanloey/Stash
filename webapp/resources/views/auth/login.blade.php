61a4379324ecb9c49e59c9a5c7aa99d7c118dcde
Arteau De Meester <arteaudemeester@gmail.com>
2018-01-22 13:48:25 +0100

    frontend login in laravel gefixed

--- webapp/resources/views/auth/login.blade.php
+++ webapp/resources/views/auth/login.blade.php
@@ -14,21 +14,23 @@
             <div class="loginWrapper">
                 {{ csrf_field() }}
                     <label for="email" class="loginLabel">{{trans('login.email')}}</label>
+                    @if ($errors->has('email'))
+                    <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+             @endif
                     <input id="email" type="email" class="textInput" name="email" value="{{ old('email') }}" required autofocus>
-                     @if ($errors->has('email'))
-                            <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                     @endif
+
 
                     <label for="password" class="loginLabel">{{trans('login.pasw')}}</label>
+                    @if ($errors->has('password'))
+                    <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                @endif
                     <input id="password" type="password" name="password" class="textInput" required>
 
-                        @if ($errors->has('password'))
-                            <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                        @endif
+
 
 
                 {{--<div class="form-group">--}}
@@ -41,11 +43,11 @@
                     {{--</div>--}}
                 {{--</div>--}}
             </div>
-            <a class="loginButtonWrapper" href="#">
+            <div class="loginButtonWrapper">
             <button type="submit" class="loginButton">
                 {{trans('login.btn')}}
             </button>
-        </a>
+        </div>
 
     </div>
     </form>
