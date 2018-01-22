2c4631fa40c2c65c0be88b3d556809366ae5e05c
jonasvanloey <jonas.vanloey@student.kdg.be>
2018-01-22 13:03:42 +0100

    return login for btn fix

--- webapp/resources/views/auth/login.blade.php
+++ webapp/resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    <form  method="POST" action="{{ route('login') }}">
     <div class="grid">
         <div class="topbar">
             <a href="#">
@@ -9,45 +10,41 @@
                 </div>
             </a>
         </div>
-        <form  method="POST" class="loginWrapper" action="{{ route('login') }}">
-            {{--<div class="loginWrapper">--}}
+
+            <div class="loginWrapper">
                 {{ csrf_field() }}
                     <label for="email" class="loginLabel">{{trans('login.email')}}</label>
-                    <div>
-                        <input id="email" type="email" class="textInput" name="email" value="{{ old('email') }}" required autofocus>
-
-                        @if ($errors->has('email'))
+                    <input id="email" type="email" class="textInput" name="email" value="{{ old('email') }}" required autofocus>
+                     @if ($errors->has('email'))
                             <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                        @endif
-                    </div>
+                     @endif
 
-                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                     <label for="password" class="loginLabel">{{trans('login.pasw')}}</label>
-                    <div>
-                        <input id="password" type="password" name="password" class="textInput" required>
+                    <input id="password" type="password" name="password" class="textInput" required>
 
                         @if ($errors->has('password'))
                             <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                         @endif
-                    </div>
-                </div>
 
-                <div class="form-group">
-                    <div>
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{trans('login.rem')}}
-                            </label>
-                        </div>
-                    </div>
-                </div>
-            {{--</div>--}}
-            <button type="submit" class="loginButtonWrapper">{{trans('login.btn')}}</button>
-        </form>
+
+                {{--<div class="form-group">--}}
+                    {{--<div>--}}
+                        {{--<div class="checkbox">--}}
+                            {{--<label>--}}
+                            {{--<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{trans('login.rem')}}--}}
+                        {{--</label>--}}
+                    {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            </div>
+            <button type="submit" class="loginButtonWrapper">
+                {{trans('login.btn')}}
+            </button>
+
     </div>
-
+    </form>
 @endsection
